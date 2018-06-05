42ac20b8f6e8b8c884fd41d9e91a1a0815b158da
mophy1109 <442377558@qq.com>
2018-06-05 19:44:02 +0800

    initial commit

---
+++ MNIST Full Connection.py
@@ -0,0 +1,232 @@
+
+# coding: utf-8
+
+# CSDN AI 挑战课 任务一:
+# 改动的部分有:
+# 隐层层数由1改成2, 隐层节点数由100增加到300
+# 初始化参数 stddev 由0.05,0.05,0.063改成0.1,0.1,0.08
+# 损失函数增加了l2正则化, 惩罚因子0.00003
+# 学习率改为动态调整,初始学习率改为0.5, decay_rate=0.91, decay_step=200
+# 最后经群里大佬指点把源代码评分部分第二个if改成elif(一个小bug)
+# 最终跑的效果正确率0.98出头, 以下内容从tinymind直接导出
+
+# 这个作业中，我们使用tensorflow来实现一个简单的手写数字识别的全连接网络，并用这个网络来做个
+# 简单的识别示例。
+# 
+# 本作业中，需要参与者应用视频中学到的知识：dropout，learingratedecay，初始化等等，将网络最终在validation数据上的得分尽可能的提高。
+# 
+# - 准确率>=98% , 100分
+# - 98%>准确率>=96% , 60分
+# - 准确率<96% , 0分
+# 
+
+# 首先导入一些用到的库。
+
+# In[1]:
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from matplotlib import pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+# 先来看看数据长什么样子
+
+# In[2]:
+
+
+mnist = input_data.read_data_sets("./")
+
+print(mnist.train.images.shape)
+print(mnist.train.labels.shape)
+
+print(mnist.validation.images.shape)
+print(mnist.validation.labels.shape)
+
+print(mnist.test.images.shape)
+print(mnist.test.labels.shape)
+
+
+# 可以看到images里面有数量不等的图片，每张图片是28x28长度的一个一维向量，
+# 所以用的时候需要先给它还原成28x28的二维图片。labels中则是图片对应的数字的值。
+
+# In[3]:
+
+
+plt.figure(figsize=(8,8))
+
+for idx in range(16):
+    plt.subplot(4,4, idx+1)
+    plt.axis('off')
+    plt.title('[{}]'.format(mnist.train.labels[idx]))
+    plt.imshow(mnist.train.images[idx].reshape((28,28)))
+
+
+# 接下来，定义用于训练的网络，首先定义网络的输入。
+# 
+# 这里我们直接使用上面的数据作为输入，所以定义两个placeholder分别用于图像和lable数据，另外，定义一个float类型的变量用于设置学习率。
+# 
+# 为了让网络更高效的运行，多个数据会被组织成一个batch送入网络，两个placeholder的第一个维度就是batchsize，因为我们这里还没有确定batchsize，所以第一个维度留空。
+
+# In[4]:
+
+
+x = tf.placeholder("float", [None, 784])
+y = tf.placeholder("int64", [None])
+learning_rate = tf.placeholder("float")
+
+
+# In[10]:
+
+
+def initialize(shape, stddev=0.1):
+  return tf.truncated_normal(shape, stddev=0.1)
+
+L1_units_count = 300
+
+W_1 = tf.Variable(initialize([784, L1_units_count], stddev=0.1))
+b_1 = tf.Variable(initialize([L1_units_count]))
+logits_1 = tf.matmul(x, W_1) + b_1
+output_1 = tf.nn.relu(logits_1)
+
+W_1_x = tf.Variable(initialize([L1_units_count, L1_units_count], stddev=0.1))
+b_1_x = tf.Variable(initialize([L1_units_count]))
+logits_1_x = tf.matmul(output_1, W_1_x) + b_1_x
+output_1_x = tf.nn.relu(logits_1_x)
+
+L2_units_count = 10 
+W_2 = tf.Variable(initialize([L1_units_count, L2_units_count], stddev=0.08))
+b_2 = tf.Variable(initialize([L2_units_count]))
+logits_2 = tf.matmul(output_1_x, W_2) + b_2  
+
+logits = logits_2
+
+
+# 接下来定义loss和用于优化网络的优化器。loss计算使用了sparse_softmax_cross_entropy_with_logits,
+# 这样做的好处是labels可以不用手动做one_hot省了一些麻烦。这里使用了sgd优化器，学习率为可以根据需要设定。
+# 
+# >试试看，增大减小学习率，换个优化器再进行训练会发生什么。
+
+# In[11]:
+
+
+cross_entropy_loss = tf.reduce_mean(
+    tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y))\
+    + tf.nn.l2_loss(W_1)*0.00003
+#     + tf.contrib.layers.l2_regularizer(0.000001)(W_2)
+
+optimizer = tf.train.GradientDescentOptimizer(
+    learning_rate=learning_rate).minimize(cross_entropy_loss)
+
+
+# 需要注意的是，上面的网络，最后输出的是未经softmax的原始logits，而不是概率分布，
+# 要想看到概率分布，还需要做一下softmax。
+# 
+# 将输出的结果与正确结果进行对比，即可得到我们的网络输出结果的准确率。
+
+# In[12]:
+
+
+pred = tf.nn.softmax(logits)
+correct_pred = tf.equal(tf.argmax(pred, 1), y)
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+# saver用于保存或恢复训练的模型。
+
+# In[13]:
+
+
+batch_size = 32
+trainig_step = 6000
+
+saver = tf.train.Saver()
+
+
+# 以上定义的所有操作，均为计算图，也就是仅仅是定义了网络的结构，实际需要运行的话，还需要创建一个session，并将数据填入网络中。
+
+# In[16]:
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    #定义验证集与测试集
+    validate_data = {
+        x: mnist.validation.images,
+        y: mnist.validation.labels,
+    }
+    test_data = {x: mnist.test.images, y: mnist.test.labels}
+
+    for i in range(trainig_step):
+        xs, ys = mnist.train.next_batch(batch_size)
+        _, loss = sess.run(
+            [optimizer, cross_entropy_loss],
+            feed_dict={
+                x: xs,
+                y: ys,
+                learning_rate: 0.5*0.91**(i/200)
+            })
+
+        #每100次训练打印一次损失值与验证准确率
+        if i > 0 and i % 100 == 0:
+            validate_accuracy = sess.run(accuracy, feed_dict=validate_data)
+            print(
+                "after %d training steps, the loss is %g, the validation accuracy is %g"
+                % (i, loss, validate_accuracy))
+            saver.save(sess, './model.ckpt', global_step=i)
+
+    print("the training is finish!")
+    #最终的测试准确率
+    acc = sess.run(accuracy, feed_dict=test_data)
+    print("the test accuarcy is:", acc)
+
+if validate_accuracy >=0.98:
+    score = 100
+if validate_accuracy >=0.96 and validate_accuracy <0.98 :
+    score = 60
+else:
+    score = 0
+print('#'*10)
+print('Your validate_accuracy:[{}]'.format(validate_accuracy))
+print('Your final score:[{}]'.format(score))
+print('#'*10)
+
+
+# 下面，用我们训练的模型做一个测试。
+
+# In[17]:
+
+
+with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state('./')
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        final_pred, acc = sess.run(
+            [pred, accuracy],
+            feed_dict={
+                x: mnist.test.images[:16],
+                y: mnist.test.labels[:16]
+            })
+        orders = np.argsort(final_pred)
+        plt.figure(figsize=(8, 8))
+        print(acc)
+        for idx in range(16):
+            order = orders[idx, :][-1]
+            prob = final_pred[idx, :][order]
+            plt.subplot(4, 4, idx + 1)
+            plt.axis('off')
+            plt.title('{}: [{}]-[{:.1f}%]'.format(mnist.test.labels[idx],
+                                                  order, prob * 100))
+            plt.imshow(mnist.test.images[idx].reshape((28, 28)))
+
+    else:
+        pass
+
+
+# 以作业提供的参数运行出来的结果，只看上面几个数字还是很不错的，但是总体准确率不太理想。
